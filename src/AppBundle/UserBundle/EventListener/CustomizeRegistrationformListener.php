8e22c9b95622d96b0ca5f59b4140afcbfb18038b
BetaDestroyer <lheinze.1992@gmail.com>
2016-05-21 13:07:51 +0200

    Eventhandler User anlegen: Mailversand inkl. Passwort

--- src/AppBundle/UserBundle/EventListener/CustomizeRegistrationformListener.php
+++ src/AppBundle/UserBundle/EventListener/CustomizeRegistrationformListener.php
@@ -8,15 +8,21 @@ use FOS\UserBundle\Event\UserEvent;
 use FOS\UserBundle\Model\UserInterface;
 use FOS\UserBundle\Util\TokenGenerator;
 use FOS\UserBundle\Form\Type\RegistrationFormType;
+use FOS\UserBundle\Mailer\MailerInterface;
 
 /**
  * Listener responsible to add custom fields to registrationform
  */
-class CustomizeRegistrationformListener implements  EventSubscriberInterface 
+class CustomizeRegistrationformListener implements  EventSubscriberInterface
 {
-	public function __construct()
-	{
 
+	protected $mailer;
+	protected $tokenGenerator;
+
+	public function __construct(\Swift_Mailer $mailer, TokenGenerator $tokenGenerator)
+	{
+		$this->mailer = $mailer;
+		$this->tokenGenerator = $tokenGenerator;
 	}
 
 	/**
@@ -30,16 +36,11 @@ class CustomizeRegistrationformListener implements  EventSubscriberInterface
         );
     }
 
-    public static function generatePass(TokenGenerator $tokenGenerator)
-    {
-        $password = substr($tokenGenerator->generateToken(), 0, 8); // 8 chars
-        return $password;
-    }
-
     public function onCreateNewCustomer(FormEvent $event)
     {
-    	// Generate Password
-    	$password = $this->generatePass($object = new TokenGenerator);
+
+    	$password = $this->tokenGenerator->generateToken();
+    	$password = substr($password, 0, 8);
 
     	// Fetch form data
     	$user = $event->getForm()->getData();
@@ -47,5 +48,16 @@ class CustomizeRegistrationformListener implements  EventSubscriberInterface
     	// Set generated Password 
     	$user->setPassword($password);
     	$user->setPlainPassword($password);
+
+    	// Send Email with new generated Password
+	    $message = \Swift_Message::newInstance()
+	            ->setSubject("Betrefftest")
+	            ->setFrom("service@awareit.com")
+	            ->setTo($user->getEmail())
+	            ->setBody("This is a test. And this is your password: ".$password."", 'text/html');
+
+	    $this->mailer->send($message);
+
     }
+
 }
